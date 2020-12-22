c0282ab1b26d98bb2032416d931602107bd5f963
Ed Bayudan <ed.bayudan@yahoo.com>
2020-12-21 22:39:53 -0500

    Use Java instance for transforming one record

--- ingester-server/src/main/java/com/ingester/transformers/GenericTransformer.java
+++ ingester-server/src/main/java/com/ingester/transformers/GenericTransformer.java
@@ -13,47 +13,30 @@ public class GenericTransformer {
 	public GenericIncomingPayload getGenericIncoming() {
 		return genericIncoming;
 	}
-	public void setGenericIncoming(GenericIncomingPayload genericIncoming) {
-		this.genericIncoming = genericIncoming;
+	public void setGenericIncoming(GenericIncomingPayload genericIncomingIn) 
+	{
+		this.genericIncoming = genericIncomingIn;
 	}
 
+	public GenericTransformer() {}
+	
 	public GenericTransformer(GenericIncomingPayload genericIncoming) throws TransformException, NoSuchMethodException{
+	}
+
+	public GenericIncomingPayload transformIncoming(GenericIncomingPayload genericIncoming) throws TransformException, NoSuchMethodException{
 		this.setGenericIncoming(genericIncoming);
 		TransformerUtils tUtils = new TransformerUtils(genericIncoming);
 		HashMap<String, String> locRef= LocReferenceDataLoader.getLocReference();
-		if (genericIncoming.getInField001().equalsIgnoreCase("1")) {
-			String errorMsg = "Error in transform";
-			this.genericIncoming.setInField050(errorMsg);		
-			throw new TransformException(errorMsg);		
+		
+		if (genericIncoming.getInField001().equalsIgnoreCase("1")) { 
+			String errorMsg = "Error in transform"; 
+			this.genericIncoming.setInField004(errorMsg); 
+			throw new TransformException(errorMsg); 
 		}
+		 
+		return this.getGenericIncoming();
 	}
 
-	/*
-	 * public GenericTransformer(GenericIncomingPayload genericIncoming) {
-	 * this.setGenericIncoming(genericIncoming);
-	 * System.out.println("GenericTransformer Constructor Input: " +
-	 * genericIncoming.toString()); HashMap<String, String> locRef=
-	 * LocReferenceDataLoader.getLocReference();
-	 * System.out.println("LocTransformer LocReferenceData:");
-	 * locRef.entrySet().forEach(entry->{ System.out.println(entry.getKey() + " " +
-	 * entry.getValue()); });
-	 * 
-	 * }
-	 */
-	/*
-	 * public LocIncoming calcPay() {
-	 * System.out.println("LocTransformer calcPay invoked"); Double pay = 2000.0;
-	 * String loc = this.locIncoming.getLOC(); boolean isNumeric =
-	 * loc.chars().allMatch( Character::isDigit ); if (isNumeric) { pay = pay +
-	 * Integer.valueOf(loc); } System.out.println("LocTransformer calcPay: " + pay +
-	 * " loc: " + loc);
-	 * this.getLocIncoming().setU_DISTRICTDESCRIPTION(pay.toString());
-	 * this.getLocIncoming().setU_DISTRICTID("C-ID");
-	 * System.out.println("LocTransformer calcPay is returning:" +
-	 * this.getLocIncoming()); return this.getLocIncoming();
-	 * 
-	 * }
-	 */
 }
 
 
